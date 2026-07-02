15f9f47b24622a59e5deecf9a9087899bf0ed1de
harshbagadai2310-cyber <harshbagadai2310@gmail.com>
2026-07-02 11:41:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:5001',
-  'http://127.0.0.1:5001'
+  'http://127.0.0.1:5001',
+  'https://www.yousayideliver.in'
 ];
 
 app.use(cors({
